f79fb7fdb37b795ac44e8c6a7db38e2e73a13e8b
truthbeauty <303040805@qq.com>
2015-02-05 11:36:51 +0800

    使用FreeImage库解析图片并绘制。

--- draw-triangle/draw-triangle/main.cpp
+++ draw-triangle/draw-triangle/main.cpp
@@ -9,15 +9,16 @@
 #include <glm/gtx/transform.hpp>
 #include <FreeImage.h>
 
+#define TEXTURE0 0
+#define LOCATION_POSITION 0
+#define LOCATION_COLOR 1
+#define LOCATION_UV 2
+
 GLuint vao;
 GLuint vbo[3];
 
-#define ATTRIB_LOCATION_POSITION 0
-#define ATTRIB_LOCATION_COLOR 1
-#define ATTRIB_LOCATION_UV 2
-
 ShaderLoader shaderLoader;
-GLuint textureLocation;
+GLuint texUnitIndex;
 
 //图片大小
 GLint width,height;
@@ -109,7 +110,7 @@ void initTriangle(void)
         1.0f,0.0f,
     };
     
-    //生成VBO
+    //生成vbo
     glGenBuffers(3, vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
@@ -120,23 +121,23 @@ void initTriangle(void)
     glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(uv_data), uv_data, GL_STATIC_DRAW);
     
-    //生成VAO，并绑定VBO
+    //生成vao,并绑定数据
     glGenVertexArrays(1,&vao);
     
         glBindVertexArray(vao);
         glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-        glVertexAttribPointer(ATTRIB_LOCATION_POSITION,3,GL_FLOAT,GL_FALSE,0,0);//指定VBO格式
+        glVertexAttribPointer(LOCATION_POSITION,3,GL_FLOAT,GL_FALSE,0,0);
         
         glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-        glVertexAttribPointer(ATTRIB_LOCATION_COLOR,3,GL_FLOAT,GL_FALSE,0,0);
+        glVertexAttribPointer(LOCATION_COLOR,3,GL_FLOAT,GL_FALSE,0,0);
         
         glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
-        glVertexAttribPointer(ATTRIB_LOCATION_UV,2,GL_FLOAT,GL_FALSE,0,0);
+        glVertexAttribPointer(LOCATION_UV,2,GL_FLOAT,GL_FALSE,0,0);
         
         //设置shader属性可访问
-        glEnableVertexAttribArray(ATTRIB_LOCATION_POSITION);
-        glEnableVertexAttribArray(ATTRIB_LOCATION_COLOR);
-        glEnableVertexAttribArray(ATTRIB_LOCATION_UV);
+        glEnableVertexAttribArray(LOCATION_POSITION);
+        glEnableVertexAttribArray(LOCATION_COLOR);
+        glEnableVertexAttribArray(LOCATION_UV);
     
     glBindVertexArray(0);
     
@@ -145,8 +146,8 @@ void initTriangle(void)
     glGenTextures(1, &mTextureID);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, mTextureID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imgBuf);
-    glUniform1i(textureLocation,0);//设置纹理单元索引
+    glTexImage2D(GL_TEXTURE_2D, TEXTURE0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imgBuf);
+    glUniform1i(texUnitIndex,TEXTURE0);//设置纹理单元索引
 
     //设置纹理过滤
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -158,16 +159,18 @@ void initTriangle(void)
 //设置并使用着色器
 void initShader(void)
 {
-    shaderLoader.load("/Users/wistoneqqx/Documents/opengl/github/texture-maps-gl6/draw-triangle/draw-triangle/simpleShader.vert",
-                      "/Users/wistoneqqx/Documents/opengl/github/texture-maps-gl6/draw-triangle/draw-triangle/simpleShader.frag");
+    shaderLoader.load("/Users/wistoneqqx/Documents/github/sbxOpenGL/textureMapping/draw-triangle/draw-triangle/simpleShader.vert",
+                      "/Users/wistoneqqx/Documents/github/sbxOpenGL/textureMapping/draw-triangle/draw-triangle/simpleShader.frag");
     shaderLoader.bind();
     
     //获取uniform变量的位置
-    textureLocation = glGetUniformLocation(shaderLoader.getProgramID(), "cube_texture");
+    texUnitIndex = glGetUniformLocation(shaderLoader.getProgramID(), "cube_texture");
 }
 
-//画三角形
-void drawTriangle(void)
+/**
+ * 绘制四边形
+*/
+void draw(void)
 {
     glBindVertexArray(vao);
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
@@ -177,7 +180,7 @@ void drawTriangle(void)
 
 int main(void)
 {
-    if(false == LoadImg("/Users/wistoneqqx/Documents/opengl/github/texture-maps-gl6/draw-triangle/draw-triangle/test1.jpg")){
+    if(false == LoadImg("/Users/wistoneqqx/Documents/github/sbxOpenGL/textureMapping/draw-triangle/draw-triangle/test1.jpg")){
         printf("加载图片失败！\n");
     }
     
@@ -215,7 +218,7 @@ int main(void)
         glClearColor(0.0f,0.0f,0.0f,0.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        drawTriangle();
+        draw();
         
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
